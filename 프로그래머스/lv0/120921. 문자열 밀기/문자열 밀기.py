5a540404fa92f1427edc67cbf0718e5c2ed3dad9
JM Lee <126068446+ltekean@users.noreply.github.com>
2023-09-17 12:19:29 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,10 @@
+def solution(A: str, B: str) -> int:
+    result = 0
+
+    while result != len(A):
+        if A == B:
+            return result
+        A = A[-1] + A[:-1]
+        result += 1
+
+    return -1
