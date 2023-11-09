b69633b20fd1157357f93971a83960ac8d490d19
JM Lee <126068446+ltekean@users.noreply.github.com>
2023-11-09 23:23:52 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,27 +1,26 @@
-while(True):
-    word=input()
+import sys
 
-    stack=[] # 괄호를 추가할 리스트 stack
-    if word=='.': # . 이 들어오면 종료
+def cal(target):
+    stack = []
+    for s in target:
+        if not len(stack) and s in [")", "]"]:
+            return "no"
+        elif s in ["(", "["]:
+            stack.append(s)
+        elif s == ")":
+            if stack[-1] != "(":
+                return "no"
+            stack.pop()
+        elif s == "]":
+            if stack[-1] != "[":
+                return "no"
+            stack.pop()
+    if not len(stack):
+        return "yes"
+    return "no"
+
+while True:
+    target = sys.stdin.readline().rstrip()
+    if target == ".":
         break
-
-    for w in word:
-        if w=='(' or w=='[': # 여는 괄호가 오면 stack에 추가
-            stack.append(w)
-        elif w==')': # ) 인 경우
-            if len(stack)!=0 and stack[-1]=='(': # stack이 비어있지 않고, 마지막 요소가 ( 이면 pop
-                stack.pop()
-            else: # stack이 비어있거나 짝이 맞지 않으면 stack에 )을 추가하고 break
-                stack.append(w)
-                break
-        elif w==']': # ] 인 경우
-            if len(stack)!=0 and stack[-1]=='[': # stack이 비어있지 않고, 마지막 요소가 [ 이면 pop
-                stack.pop()
-            else: # stack이 비어있거나 짝이 맞지 않으면 stack에 ]을 추가하고 break
-                stack.append(w)
-                break
-
-    if len(stack)==0: # stack이 비어있으면 모든 괄호들의 균형이 맞으므로 Yes 출력
-        print('yes')
-    else: # stack이 비어있지 않으면 괄호들의 균형이 맞지 않는 것이므로 No 출력
-        print('no')
+    print(cal(target))
