0f7d21db71501200156b1740941990adac50b2d2
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-03-14 21:54:59 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,18 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                total = nums[i] + nums[j] + nums[k]
+                if is_prime(total):
+                    answer += 1
+    return answer
