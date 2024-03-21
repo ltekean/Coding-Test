2e8da153c1cd914761a54f2d7403fbb6b4d7636e
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-03-21 14:21:02 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,10 @@
+def solution(n, slicer, num_list):
+    answer = []
+    a,b,c = slicer[0], slicer[1], slicer[2]
+    if n == 1:
+        return num_list[:b+1]
+    elif n == 2:
+        return num_list[a:]
+    elif n == 3:
+        return num_list[a:b+1]
+    return num_list[a:b+1:c]
