e503d827f7cb77a68ad229cbf8d4c0a21bc4faea
JM Lee <126068446+ltekean@users.noreply.github.com>
2024-03-21 14:14:21 +0900

    [level 0] Title: 세 개의 구분자, Time: 10.37 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,16 +1,3 @@
 def solution(myStr):
-    result = []
-    temp = ""
-    for char in myStr:
-        if char in "abc":
-            if temp:
-                result.append(temp)
-                temp = ""
-        else:
-            temp += char
-    if temp:
-        result.append(temp)
-    
-    if not result:
-        return ["EMPTY"]
-    return result
+    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+    return answer if answer else ['EMPTY']
